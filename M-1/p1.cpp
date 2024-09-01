28ff3e60155641fa2d70c7e4fd3f19121c439b05
Asik8 <motasembillah84@gmail.com>
2024-09-01 22:37:57 +0600

    Adjacency matrix convertion done

---
+++ M-1/p1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,e;
+    cin>>n>>e;
+    int a[n][n];
+    memset(a,0,sizeof(a));
+    while(e--)
+    {
+        int l,r;
+        cin>>l>>r;
+        a[l][r] = 1;
+        a[r][l] = 1;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++) cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
