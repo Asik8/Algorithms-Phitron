912bd4ab72a8c17dc55dc1f83e9a79b9d0baa97e
Asik8 <motasembillah84@gmail.com>
2024-09-05 19:04:31 +0600

    Count No of Components using DFS

--- M-3.5/p2.cpp
+++ M-3.5/p2.cpp
@@ -7,6 +7,18 @@ const ll N = 1e5+10;
 vector<ll> v[N];
 bool vis[N];
 
+void DFS(ll n)
+{
+    vis[n] = true;
+    for(ll x:v[n])
+    {
+        if(!vis[x])
+        {
+            DFS(x);
+        }
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -19,9 +31,15 @@ int main() {
         v[a].pb(b);
         v[b].pb(a);
     }
+    ll c=0;
     for (int i=0;i<n;i++)
     {
-        
-    }       
+        if(!vis[i])
+        {
+            c++;
+            DFS(i);
+        }
+    }   
+    cout<<c<<endl;    
     return 0;
 }
