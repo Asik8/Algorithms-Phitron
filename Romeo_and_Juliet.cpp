7a7f93be02142aa85b114aedecffe0e577324d89
Asik8 <motasembillah84@gmail.com>
2024-09-25 19:11:11 +0600

    p1 done Using BFS

---
+++ Romeo_and_Juliet.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+const int N = 200005;
+vector<vector<int>>Edges(N);
+bool vis[N];
+int dis[N];
+
+void bfs(int s)
+{
+    queue<int>q;
+    q.push(s);
+    vis[s] = true;
+    dis[s] = 0;
+    while(!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        for(int c:Edges[par])
+        {
+            if(!vis[c])
+            {
+                vis[c] = true;
+                dis[c] = dis[par]+1;
+                q.push(c);
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,e;
+    cin>>n>>e;
+    while(e--)
+    {
+        int a,b;
+        cin>>a>>b;
+        Edges[a].pb(b);
+        Edges[b].pb(a);
+    }
+    int x,y,k;
+    cin>>x>>y>>k;
+    memset(vis,false,sizeof(vis));
+    memset(dis,-1,sizeof(dis));
+    bfs(x);
+    if(dis[y] != -1 && dis[y]<=k*2) py
+    else pn
+    return 0;
+}
